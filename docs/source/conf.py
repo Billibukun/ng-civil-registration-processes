52fceea13101cbbf5fac9be7d586cf5fee2e7527
Ibukunoluwa Omonijo <billibukun@gmail.com>
2024-06-21 18:08:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Civil Registration and Vital Statistics System'
+copyright = '2024, National Population Commisison'
+author = 'Ibukunoluwa Omonijo, Information Developer'
 
 release = '0.1'
 version = '0.1.0'
